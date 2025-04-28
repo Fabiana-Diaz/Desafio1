bf36d0d17d52e7e962aea43a703a8f76a315c3a5
Fabiana-Diaz <fabiana.diaz@udea.edu.co>
2025-04-27 23:08:57 -0500

    Update nombre_de_funciones.h

--- nombre_de_funciones.h
+++ nombre_de_funciones.h
@@ -1,17 +1,16 @@
-
 #ifndef NOMBRE_DE_FUNCIONES_H
 #define NOMBRE_DE_FUNCIONES_H
 
 #include <QString>
 
-// Funciones dadas por el profe.
+// Funciones originales (NO cambiar nombres)
 unsigned char* loadPixels(QString input, int &width, int &height);
 bool exportImage(unsigned char* pixelData, int width, int height, QString archivoSalida);
 unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
 
-//funcion para comparar valores
+// Nuevas funciones mejoradas
 bool compararValores(int valorImagen, int valorTxt);
-//funciones para verificar y aplicar xor
+
 bool verificarXOR(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int tamMascara, int semilla);
 bool aplicarXOR(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int totalBytes, int tamMascara, int semilla, int orden);
 
@@ -22,5 +21,7 @@ bool verificarDesplazamiento(unsigned char* img, unsigned char* mascara, unsigne
 bool aplicarDesplazamiento(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, int totalBytes, int tamMascara, int semilla, int nBits, bool izquierda, int orden);
 
 void aplicarTransformaciones(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int totalBytes, int tamMascara, int semilla, int orden);
+
+void procesarArchivos(int nArchivos, unsigned char* img, unsigned char* mascara, unsigned char* imgXor, int totalBytes);
 
 #endif // NOMBRE_DE_FUNCIONES_H
