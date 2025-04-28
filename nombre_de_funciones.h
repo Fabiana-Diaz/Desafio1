0c470e1f5f44abbc25c89d722df4758b199cdff8
Fabiana-Diaz <fabiana.diaz@udea.edu.co>
2025-04-27 22:51:22 -0500

    Update nombre_de_funciones.h
    se agregan los nombres de las nuevas funciones, aplicar rotación y verifificar rotacion

--- nombre_de_funciones.h
+++ nombre_de_funciones.h
@@ -15,4 +15,7 @@ bool compararValores(int valorImagen, int valorTxt);
 bool verificarXOR(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int tamMascara, int semilla);
 bool aplicarXOR(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int totalBytes, int tamMascara, int semilla, int orden);
 
+bool verificarRotacion(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, int tamMascara, int semilla, int nBits, bool izquierda);
+bool aplicarRotacion(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, int totalBytes, int tamMascara, int semilla, int nBits, bool izquierda, int orden);
+
 #endif // NOMBRE_DE_FUNCIONES_H
