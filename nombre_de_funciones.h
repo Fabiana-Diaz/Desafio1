d4580ffa289fe8704e4d34f42c3267be1da91c13
Fabiana-Diaz <fabiana.diaz@udea.edu.co>
2025-04-27 22:35:58 -0500

    Update nombre_de_funciones.h
    se agregan los nombres de las nuevas funciones

--- nombre_de_funciones.h
+++ nombre_de_funciones.h
@@ -1,16 +1,18 @@
+
 #ifndef NOMBRE_DE_FUNCIONES_H
 #define NOMBRE_DE_FUNCIONES_H
 
-#include <fstream>
-#include <iostream>
-#include <QCoreApplication>
-#include <QImage>
+#include <QString>
 
-using namespace std;
-
-unsigned char* aplicarXOR(unsigned char* imagen1, unsigned char* imagen2, int totalBytes);
+// Funciones dadas por el profe.
 unsigned char* loadPixels(QString input, int &width, int &height);
 bool exportImage(unsigned char* pixelData, int width, int height, QString archivoSalida);
 unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
+
+//funcion para comparar valores
+bool compararValores(int valorImagen, int valorTxt);
+//funciones para verificar y aplicar xor
+bool verificarXOR(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int tamMascara, int semilla);
+bool aplicarXOR(unsigned char* img, unsigned char* mascara, unsigned int* datosTxt, unsigned char* imgXor, int totalBytes, int tamMascara, int semilla, int orden);
 
 #endif // NOMBRE_DE_FUNCIONES_H
